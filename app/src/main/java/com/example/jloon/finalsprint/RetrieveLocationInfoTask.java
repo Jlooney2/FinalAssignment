2157a9983332f8c12a755151d2a7f8311c8aed44
jloon <jlooney2@my.westga.edu>
2018-04-28 23:14:43 -0400

    Parsed json not finished but working

--- app/src/main/java/com/example/jloon/finalsprint/RetrieveLocationInfoTask.java
+++ app/src/main/java/com/example/jloon/finalsprint/RetrieveLocationInfoTask.java
@@ -1,8 +1,10 @@
 package com.example.jloon.finalsprint;
 
 import android.os.AsyncTask;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -12,11 +14,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class RetrieveLocationInfoTask extends AsyncTask<URL, String, String> {
 
     private String summary;
     private  int count = 0;
+    private ArrayList<Object> infoCollection;
     @Override
     protected String doInBackground(URL... urls) {
         HttpURLConnection request = null;
@@ -28,9 +32,20 @@ public class RetrieveLocationInfoTask extends AsyncTask<URL, String, String> {
             JsonParser jp = new JsonParser(); //from gson
             JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
             JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+            JsonArray infoArray = rootobj.getAsJsonArray("geonames");
 
-            summary = String.valueOf(rootobj.size());
-            // summary = rootobj.get("lat").getAsString();
+            this.infoCollection = new ArrayList<Object>();
+            for(Object curr : infoArray){
+                this.infoCollection.add(curr.toString());
+            }
+            StringBuilder info = new StringBuilder();
+            for (Object curr: this.infoCollection){
+                info.append(curr + System.lineSeparator());
+            }
+
+            summary = info.toString();
+            System.out.println(summary);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
